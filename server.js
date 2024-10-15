c922e1dd4bf4bacc9c2c954b2287b7c4d4757d9a
Sara <sara_babic@intention.de>
2024-10-15 21:36:23 +0200

    Working with mongoose db

--- server.js
+++ server.js
@@ -6,8 +6,7 @@ const app = express()
 mongoose.connect('mongodb://localhost/blog', { useNewUrlParser: true, useUnifiedTopology: true })
 
 app.set('view engine', 'ejs')
-
-app.use('/articles',articleRouter)
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
 
@@ -23,5 +22,7 @@ app.get('/', (req, res) => {
     }]
     res.render('articles/index', { articles: articles})
 })
+
+app.use('/articles',articleRouter)
 
 app.listen(5001)
