8eba95ddef797e8320cc38f8ebeb36ee3e516993
Sara <sara_babic@intention.de>
2024-10-16 21:47:38 +0200

    create

--- routes/articles.js
+++ routes/articles.js
@@ -6,12 +6,15 @@ router.get('/new', (req, res) => {
     res.render('articles/new', { article: new Article() })
   })
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', async(req, res) => {
+    const article = await Article.findById(req.params.id)
+    if (article == null) res.redirect('/')
+    res.render('articles/show', { article: article});
+    res.send(req.params.id)
 })
 
 router.post('/', async (req, res) => {
-    const article = new Article({
+    let article = new Article({
         title: req.body.title,
         description: req.body.description,
         markdown: req.body.markdown,
@@ -20,6 +23,7 @@ router.post('/', async (req, res) => {
         article = await article.save();
         res.redirect(`/articles/${article.id}`)
     } catch (e) {
+        console.log('ERROR :::', e)
         res.render('articles/new', { article: article })
     }
     
