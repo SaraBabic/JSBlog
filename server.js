daf64f65bff6b08e57c61ea64c7c9b111721edc2
Sara <sara_babic@intention.de>
2024-10-22 15:12:02 +0200

    Slug, delete and show article page

--- server.js
+++ server.js
@@ -1,25 +1,19 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const articleRouter = require('./routes/articles')
+const Articles = require('./models/article')
+const methodOverride = require('method-override')
 const app = express()
 
-mongoose.connect('mongodb://localhost/blog', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost:27017/blog', {  })
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
 
-    const articles = [{
-        title: 'Test Article',
-        createdAt: new Date(),
-        description: 'Lorem ipsum dolor sit amet.'
-    },
-    {
-        title: 'Test Article 2',
-        createdAt: new Date(),
-        description: 'Lorem ipsum dolor sit amet.'
-    }]
+    const articles = await Articles.find().sort({ createdAt: "desc"})
     res.render('articles/index', { articles: articles})
 })
 
