71839b53ffa063385be9ff345bcbbe22687d647b
Sara <sara_babic@intention.de>
2024-10-29 20:15:27 +0100

    Api routes for login and registration

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const Articles = require('./models/article')
 const Article = require('./models/article')
 const methodOverride = require('method-override')
 const app = express()
+const User = require('./models/user')
+const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
+
 
 mongoose.connect('mongodb://localhost:27017/blog', {  })
 
@@ -103,9 +107,58 @@ app.delete("/api/articles/:id", async (req, res) => {
   }
 });
 
+app.post('/api/register', async (req, res) => {
+  const { name, email, password } = req.body;
 
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: 'User already exists' });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    console.error("Registration error:", error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.post('/api/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ error: 'Invalid email or password' });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(400).json({ error: 'Invalid email or password' });
+    }
+
+    const token = jwt.sign({ id: user._id }, 'your_secret_key', { expiresIn: '1h' });
+
+    res.json({ message: 'Login successful', token });
+  } catch (error) {
+    console.error('Login error:', error);
+    res.status(500).json({ error: 'An error occurred during login' });
+  }
+});
 
 
 app.use('/articles',articleRouter)
 
-app.listen(5001)
+app.listen(5001, () => {
+  console.log("server is running")
+})
